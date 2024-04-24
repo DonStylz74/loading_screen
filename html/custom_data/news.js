22d6ddd71786248747c6855565532ee2d572d4c2
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-24 19:50:25 +1000

    Add files via upload

---
+++ html/custom_data/news.js
@@ -0,0 +1,15 @@
+var News = {
+	
+	contents: [
+        {img: 'news-1.jpg', title:'Game Assigned Key Controls', data:'16.06.2021', text:'[F1] -- Not Assigned, | [F2] -- Player Inventory, | [F3] -- Player Animations. | [F4] -- Vehicle Menu, | [F5] -- Player Menu, | [F6] -- Job menu. | [F7] -- Billing Menu, | [F9] -- Not Assigned, | [F10] -- Not Assigned. | [F11] -- Not Assigned, latest updates, cool design.' },
+        {img: '', title:'Latest updates', data:'11.05.2021', text:'Server news, latest updates, cool design. Server news, latest updates, cool design.' },
+        {img: '', title:'Cool design', data:'11.05.2021', text:'Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design.' },
+        {img: 'news-3.gif', title:'Crazy admins', data:'11.05.2021', text:'ATTENTION! ATTENTION! ATTENTION! admins are crazy. Stay at home, avoid popular places and a lot of people! admins are crazy. Stay at home, avoid popular places and a lot of people! admins are crazy. Stay at home, avoid popular places and a lot of people!' },
+        {img: '', title:'Server news', data:'11.05.2021', text:'Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design.' },
+        {img: '', title:'Latest updates', data:'11.05.2021', text:'Server news, latest updates, cool design.' },
+        {img: '', title:'Cool design', data:'11.05.2021', text:'Server news, latest updates, cool design. Server news, latest updates, cool design.' },
+        {img: 'news-2.gif', title:'Crazy admins', data:'11.05.2021', text:'Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design. Server news, latest updates, cool design.' },
+        {img: '', title:'Lowrider transport is played', data:'11.05.2021', text:'Server news, latest updates, cool design. ' },
+        {img: '', title:'Server news', data:'11.05.2021', text:'Server news, latest updates, cool design.' }
+    ],
+};
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://streamable.com/lzmo1h
